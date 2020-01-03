1ac5bb00dbbbe818f9b2b3ae2adb17a6076f16f1
ashley George <aogeorge545@gmail.com>
2020-01-02 19:03:01 -0800

    Completed the visual desig of the Recipe webpage, but without Javascript functionalities attached to the page.

--- Pages/Recipe.js
+++ Pages/Recipe.js
@@ -1,24 +1,15 @@
+$('[data-search]').on('keyup', function(){
+    var searchVal = $(this).val();
+    var filterItems = $('[data-filter-item]');
 
-$( ".recipe" ).click(function(){
+    if(searchVal != ''){
+        filterItems.addClass('hidden');
+        $('[data-filter-item][data-filter-name*="'+ 
+            searchVal.toLowerCase()+ '"]').removeClass('hidden');
 
-    var getLinkType, getElType, getElNotType;
-  
-    // Feature-detect for dataset support
-    if( !this.dataset ) { // If IE 10 or lower
-      getLinkType = this.getAttribute( "data-item" );
-     } else { // For other browsers
-       getLinkType = this.dataset.team;
-     }
-  
-    getElType = $( "div[data-food-item~="+getLinkType+"]" );
-  
-    getElNotType = $( "div[data-food-item!="+getLinkType+"]" );
-  
-     $( ".food" ).filter( getElNotType ).css( "display", "none" );
-     $( ".food" ).filter( getElType ).css( "display", "block" );
-  
-  });
-  
-  $( ".recipe-all" ).click(function() {
-    $( ".food" ).css( "display", "block" );
-  });
+        }else {
+            filterItems.removeClass('hidden');
+        }
+
+
+});
